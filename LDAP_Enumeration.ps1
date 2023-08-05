4d36a8d43da52aa190365b5abc56ef53e8144b14
PJ Frost <blueminty100@gmail.com>
2023-08-05 16:51:20 -0400

    Create LDAP_Enumeration.ps1

---
+++ LDAP_Enumeration.ps1
@@ -0,0 +1,6 @@
+# Using .NET classes and ADSI to dynamically obtain the full LDAP path
+
+$PDC = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
+$DN = ([adsi]'').distinguishedName 
+$LDAP = "LDAP://$PDC/$DN"
+$LDAP
